de3b4ca5079d9988059fa507ff2999c5b8880f21
nizar <brigui.nizar@gmail.com>
2025-03-11 03:35:06 +0100

    rm node_modules

--- exemple-webcomponent/app/src/main.ts
+++
@@ -1,4 +0,0 @@
-import './component/navbar.ts';
-import './component/footer-component.ts';
-/* import './component/pong-game.ts'; */
-import './component/app-wrapper.ts';
--- exemple-webcomponent/scripts/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cp -R /node_modules /app
-
-npm i
-npm run build
-exec npm run dev
