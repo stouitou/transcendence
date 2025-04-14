72053336528a4ea2c35c37f7412c8a71cb5f0f85
Olivier COSTE <olcoste@bess-f4r1s3.clusters.42paris.fr>
2025-04-14 18:50:28 +0200

    fds

--- frontend-pong-module/app/src/display/menu.ts
+++ frontend-pong-module/app/src/display/menu.ts
@@ -4,7 +4,9 @@ enum Size {
 	WidthOffset = 2,
 	HeightOffset = 3,
 	PoliceText = 4,
-	StringText = 5
+	StringText = 5,
+	ColorBackground = 6,
+	ColorText = 7
 }
 
 export class menu {
@@ -12,7 +14,7 @@ export class menu {
 	//private _guestName!: string;
 
 	private _outline!: HTMLElement;
-	private _button!: HTMLButtonElement;
+	private _button: HTMLButtonElement[] = [];
 	private _buttonMenu!: HTMLButtonElement;
 
 	private readonly _widthButton: number = 200;
@@ -31,7 +33,7 @@ export class menu {
 	private _texte!: HTMLDivElement;
 	private _li!: HTMLLIElement;
 
-	private _input!: HTMLInputElement;
+	private _input: HTMLInputElement[] = [];
 
 	private _textButton: string[][] = [];
 
@@ -66,30 +68,30 @@ export class menu {
 	}
 
 	//afficher les boutons, marche avec displayMenu(textButton: string[])
-	public displayButton(textButton: string[]) : Promise<void>
+	public displayButton(x: number, textButton: string[]) : Promise<void>
 	{
 		return new Promise((resolve) => {
 			this._spaceButtons = window.innerHeight /  2 - this._heightMenu / 2 + this._heightButton * 0.2;
 
 			for (let x = 0; x < textButton.length; x++)
 			{
-				this._button = document.createElement("button");
+				this._button[x] = document.createElement("button");
 
-				this._button.style.position = "absolute";
-				this._button.style.backgroundColor = "rgb(255, 0, 0)";
-				this._button.style.top = `${this._spaceButtons}px`;
+				this._button[x].style.position = "absolute";
+				this._button[x].style.backgroundColor = "rgb(255, 0, 0)";
+				this._button[x].style.top = `${this._spaceButtons}px`;
 				this._spaceButtons += (this._heightButton * 0.2);
-				this._button.style.padding = "0px";
-				this._button.style.width = `${this._widthButton}px `;
-				this._button.style.height = `${this._heightButton}px`;
+				this._button[x].style.padding = "0px";
+				this._button[x].style.width = `${this._widthButton}px `;
+				this._button[x].style.height = `${this._heightButton}px`;
 			
-				this._button.textContent = textButton[x];
+				this._button[x].textContent = textButton[x];
 
 				this._spaceButtons += this._heightButton;
 
-				document.body.appendChild(this._button);
+				document.body.appendChild(this._button[x]);
 
-				this._button.addEventListener("click", (event) => {
+				this._button[x].addEventListener("click", (event) => {
 					const buttons: HTMLButtonElement[] = Array.from(document.querySelectorAll("button"));
 					this._outline.remove();
 					buttons.forEach(button => button.remove());
@@ -181,11 +183,11 @@ export class menu {
 		this._container[i].style.left = `calc(${window.innerWidth /  2}px - ${Number(tab[Size.Width]) / 2}px + ${Number(tab[Size.WidthOffset])}px)`;
 	  
 		// Création du bouton
-		this._buttonMenu = document.createElement("button");
-		this._buttonMenu.textContent = String(tab[Size.StringText]);
-		this._buttonMenu.style.height = `${tab[Size.Height]}px`;
-		this._buttonMenu.style.width = `${tab[Size.Width]}px`;
-		this._buttonMenu.style.padding = "0px";
+		let buttonMenu = document.createElement("button");
+		buttonMenu.textContent = String(tab[Size.StringText]);
+		buttonMenu.style.height = `${tab[Size.Height]}px`;
+		buttonMenu.style.width = `${tab[Size.Width]}px`;
+		buttonMenu.style.padding = "0px";
 	  
 		// Création du menu (liste)
 		const menu: HTMLUListElement = document.createElement("ul");
@@ -205,88 +207,90 @@ export class menu {
 		// Création des items du menu
 		const items: string[] = std;
 		items.forEach(itemText => {
-		  this._li = document.createElement("li");
-		  this._li.textContent = itemText;
-		  this._li.style.width = `${tab[Size.Width]}px`;
-		  this._li.style.height = `${tab[Size.Height]}px`;
-		  this._li.style.padding = "0px";
-		  this._li.style.margin = "0px";
-		  this._li.style.border = "none"
-		  this._li.style.cursor = "pointer";
+		this._li = document.createElement("li");
+
+		this._li.textContent = itemText;
+		this._li.style.width = `${tab[Size.Width]}px`;
+		this._li.style.height = `${tab[Size.Height]}px`;
+		this._li.style.padding = "0px";
+		this._li.style.margin = "0px";
+		this._li.style.border = "none"
+		this._li.style.cursor = "pointer";
 	  
-		  // Ajout d'effets au survol
-		  this._li.addEventListener("mouseover", () => this._li.style.backgroundColor = "#eee");
-		  this._li.addEventListener("mouseout", () => this._li.style.backgroundColor = "#fff");
-	  
-		  // Lorsqu'un item est cliqué, on log l'option et on masque le menu
-		  this._li.addEventListener("click", () => {
-			//console.log(`Élément sélectionné : ${itemText}`);
-			//this._container[i].remove();
-		menu.style.display = "none";
-			console.log("Option du menu deroulant:",Number(itemText));
+		// Ajout d'effets au survol
+		this._li.addEventListener("mouseover", () => this._li.style.backgroundColor = "#eee");
+		this._li.addEventListener("mouseout", () => this._li.style.backgroundColor = "#fff");
+
+		// Lorsqu'un item est cliqué, on log l'option et on masque le menu
+		this._li.addEventListener("click", () => {
+			buttonMenu.textContent = itemText;
+			menu.style.display = "none";
+			//console.log("Option du menu deroulant:",Number(itemText));
 			onSelect(itemText);
+			
+			//this._container[i].appendChild(buttonMenu);
 		  });
 		  
 		  menu.appendChild(this._li);
 		});
 	  
 		// Ajout d'un écouteur sur le bouton pour basculer l'affichage du menu
-		this._buttonMenu .addEventListener("click", () => {
+		buttonMenu.addEventListener("click", () => {
 		menu.style.display = (menu.style.display === "none") ? "block" : "none";
 		});
-
+		//console.log("Appuie sur le menu deroulant");
 		// Regroupement des éléments
-		this._container[i].appendChild(this._buttonMenu );
+		this._container[i].appendChild(buttonMenu);
 		this._container[i].appendChild(menu);
 		document.body.appendChild(this._container[i]);
 	}
 
 	
 	//creer un champ input avec son bouton de validation
-	public createTextInputCustom(...tab: (string | number)[])
+	public createTextInputCustom(x: number, ...tab: (string | number)[])
 	{
-		this._input = document.createElement("input");
+		this._input[x] = document.createElement("input");
 
-		this._input.type = "text";
-		this._input.style.position = "absolute";
-		this._input.placeholder = String(tab[Size.StringText]);
+		this._input[x].type = "text";
+		this._input[x].style.position = "absolute";
+		this._input[x].placeholder = String(tab[Size.StringText]);
 	
-		this._button = document.createElement("button");
-		this._button.style.position = "absolute";
-		this._button.textContent = "Ok";
-		this._button.style.padding = "0px";
-		this._button.style.top = `calc(${window.innerHeight /  2}px - ${Number(tab[Size.Height]) / 2}px + ${tab[Size.HeightOffset]}px)`;
-		this._button.style.left = `calc(${window.innerWidth /  2}px - ${Number(tab[Size.Width]) / 2}px + ${tab[Size.WidthOffset]}px + ${Number(tab[Size.Width]) + 10}px)`;
+		this._button[x] = document.createElement("button");
+		this._button[x].style.position = "absolute";
+		this._button[x].textContent = "Ok";
+		this._button[x].style.padding = "0px";
+		this._button[x].style.top = `calc(${window.innerHeight /  2}px - ${Number(tab[Size.Height]) / 2}px + ${tab[Size.HeightOffset]}px)`;
+		this._button[x].style.left = `calc(${window.innerWidth /  2}px - ${Number(tab[Size.Width]) / 2}px + ${tab[Size.WidthOffset]}px + ${Number(tab[Size.Width]) + 10}px)`;
 		
-		this._input.style.padding = "0px";
-		this._input.style.height = `${tab[Size.Height]}px`;
-		this._input.style.width = `${tab[Size.Width]}px`;
-		this._input.style.top = `calc(${window.innerHeight /  2}px - ${Number(tab[Size.Height]) / 2}px + ${tab[Size.HeightOffset]}px)`;
-		this._input.style.left = `calc(${window.innerWidth /  2}px - ${Number(tab[Size.Width]) / 2}px + ${tab[Size.WidthOffset]}px)`;		
+		this._input[x].style.padding = "0px";
+		this._input[x].style.height = `${tab[Size.Height]}px`;
+		this._input[x].style.width = `${tab[Size.Width]}px`;
+		this._input[x].style.top = `calc(${window.innerHeight /  2}px - ${Number(tab[Size.Height]) / 2}px + ${tab[Size.HeightOffset]}px)`;
+		this._input[x].style.left = `calc(${window.innerWidth /  2}px - ${Number(tab[Size.Width]) / 2}px + ${tab[Size.WidthOffset]}px)`;		
 
-		document.body.appendChild(this._input);
-		document.body.appendChild(this._button);
+		document.body.appendChild(this._input[x]);
+		document.body.appendChild(this._button[x]);
 	}
 
 	//afficher les boutons custom
-	public displayButtonCustom(...tab: (string | number)[]) : Promise<void>
+	public displayButtonCustom(x: number, ...tab: (string | number)[]) : Promise<void>
 	{
 		return new Promise((resolve) => {
-				this._button = document.createElement("button");
+				this._button[x] = document.createElement("button");
 
-				this._button.style.position = "absolute";
-				this._button.style.fontFamily = `${tab[Size.PoliceText]}`;
-				this._button.style.padding = "0px";
-				this._button.style.backgroundColor = "rgb(179, 95, 95)";
-				this._button.style.top =  `calc(${window.innerHeight /  2}px - ${Number(tab[Size.Height]) / 2}px + ${tab[Size.HeightOffset]}px)`;
-				this._button.style.left = `calc(${window.innerWidth /  2}px - ${Number(tab[Size.Width])  / 2}px + ${tab[Size.WidthOffset]}px)`;
-				this._button.style.width = `${tab[Size.Width]}px`;
-				this._button.style.height = `${tab[Size.Height]}px`;
-				this._button.textContent = String(tab[Size.StringText]);
+				this._button[x].style.position = "absolute";
+				this._button[x].style.fontFamily = `${tab[Size.PoliceText]}`;
+				this._button[x].style.padding = "0px";
+				this._button[x].style.backgroundColor = "rgb(179, 95, 95)";
+				this._button[x].style.top =  `calc(${window.innerHeight /  2}px - ${Number(tab[Size.Height]) / 2}px + ${tab[Size.HeightOffset]}px)`;
+				this._button[x].style.left = `calc(${window.innerWidth /  2}px - ${Number(tab[Size.Width])  / 2}px + ${tab[Size.WidthOffset]}px)`;
+				this._button[x].style.width = `${tab[Size.Width]}px`;
+				this._button[x].style.height = `${tab[Size.Height]}px`;
+				this._button[x].textContent = String(tab[Size.StringText]);
 
-				document.body.appendChild(this._button);
+				document.body.appendChild(this._button[x]);
 
-				this._button.addEventListener("click", (event) => {
+				this._button[x].addEventListener("click", (event) => {
 					const buttons: HTMLButtonElement[] = Array.from(document.querySelectorAll("button"));
 					this._outline.remove();
 					buttons.forEach(button => button.remove());
@@ -302,7 +306,7 @@ export class menu {
 	private async display(textButton: string[])
 	{
 		this.displayMenu(textButton);
-		await this.displayButton(textButton);
+		await this.displayButton(0, textButton);
 	}
 
 	public async center()
@@ -318,6 +322,9 @@ export class menu {
 			case 'Tournoi' :
 				this.Tournoi();
 				break;
+			case 'Distant' :
+				this.distant();
+				break;
 			case 'Return' :
 				await this.display(this._textButton[0]);
 				break;
@@ -326,6 +333,7 @@ export class menu {
 
 	private checkNameGuest(guestName: string)
 	{
+		//console.log("name :", guestName);
 		if (this._texte && document.body.contains(this._texte)) {
 			this._texte.remove();
 		}
@@ -345,38 +353,51 @@ export class menu {
 		return(true);
 	}
 
+	public async distant() : Promise<void>
+	{
+		this.displayMenuCustom(200, 50);
+		this.createTextInputCustom(0, 140, 20, -20, 0, "Courier", "Pseudo du joueur");
+		this._button[0].addEventListener("click", () => {
+				console.log("Name du guest valide : ", this._input[0].value); //chercher si le pseudo existe dans la data, lancer la fonction game ici, clean menu
+		});	
+	}
+
 	public async localGame() : Promise<void>
 	{
 		const std: string[] = ["Facile", "Moyen", "Hard"];
 		return new Promise((resolve) => {
-			this.createTextInputCustom(140, 20, -60, 0, "Courier", "Pseudo de l'invite");
+			this.createTextInputCustom(0, 140, 20, -60, 0, "Courier", "Pseudo de l'invite");
 			this.SmallCheckboxCustom(110, 0, 20, 20);
 			this.displayMenuCustom(270, 50);
 
-			this._button.addEventListener("click", () => {
-				console.log(this._input.value);
-				if (this.checkNameGuest(this._input.value))
-					console.log("Name du guest valide"); //lancer la fonction game ici
+			this._button[0].addEventListener("click", () => {
+				//console.log(this._input.value);
+				if (this.checkNameGuest(this._input[0].value))
+				{
+					//this._button.remove();
+					console.log("Name du guest valide : ", this._input[0].value); //lancer la fonction game ici, clean menu
+				}
 			});		
 
 			this._checkbox.addEventListener("change", () => {
 			if (this._checkbox.checked) {
-				this._input.remove();
-				this._button.remove();
+				this._input[0].remove();
+				this._button[0].remove();
 				this.DropdownMenu(std, 0, [150, 40, -50, 0 , "Arrial", "Choose your LVL"], (val) => {
-					
-					this._buttonMenu.textContent = String(val);
+					this.displayButtonCustom(0, 20, 20, 40, 0, "Courier", "OK");
 				});
 			}
 			else {
-				console.log("tet");
+				//console.log("tet");
 				this._container[0].remove();
+				if (this._button[0])
+					this._button[0].remove();
 				// this._input.remove();
 				// this._button.remove();
 				//this.displayButtonCustom(30, 30, 35, 0,  "Courier", "Ok");
 		
 				/* METRE UN VALIDE CUSTOM ET LE RETIRER DE createTextInputCustom*/
-				this.createTextInputCustom(140, 20, -60, 0, "Courier", "Pseudo de l'invite");
+				this.createTextInputCustom(0, 140, 20, -60, 0, "Courier", "Pseudo de l'invite");
 			}
 			resolve();
 			})
@@ -387,19 +408,77 @@ export class menu {
 		})
 	}
 
-	// public Tournoi()
+	public Tournoi() //trouver comment enlever le padding et recalculer pour tout bien psotionner
+	{
+		//let std: string[] = ["3", "4", "5", "6", "7", "8"];
+	
+		//console.log("Menu = ", 50 + (50 * this.n));
+		this.displayMenuCustom(400, 50 + (40 * this.n));
+
+		this.TextDisplayCustom(80, 25, -150, (-20 * this.n), "Arial", "Choose your number of players: ");
+		//console.log("(-25 * this.n) = ",(-25 * this.n));
+
+		//console.log("");
+		this.DropdownMenu(["3", "4", "5", "6", "7", "8"], 0, [80, 30, 115, ((-20 * this.n)), "Arial", `${this.n + 1}`], (val) => {
+			//if (this.n != 0)
+			this._container[0].remove();
+			for (let x = 0; x < this._input.length; x++) {
+				this._input[x].remove(); 
+				this._button[x].remove();
+			}
+
+		this.n = Number(val) - 1;
+			
+			for (let i = 0; i < this.n; i++)
+			{
+				let sup: number = 0
+				if (this.n != 2)
+					sup = 5 * (Number(val) - 2);
+				if (i === this.n - 1 && (this.n + 1) % 2 === 1)
+				{
+					console.log("i = ", i);
+					this.SmallCheckboxCustom(60, (-10 * (Number(val)) - sup) + (40 * i), 20, 20)
+				}
+				this.createTextInputCustom(i, 140, 20,  -100, (-10 * (Number(val)) - sup) + (40 * i), "Courier", "Pseudo de l'invite");
+
+				this._button[i].addEventListener("click", () => {
+				console.log("i = ", i);
+				console.log("Name du guest valide : ", this._input[i].value); //chercher si le pseudo existe dans la data, lancer la fonction game ici, clean menu
+				});	
+				
+			}
+			if ((this.n + 1) % 2 === 1)
+			{
+			this._checkbox.addEventListener("change", () => {
+				this._button[this._button.length - 1].remove();
+				this._input[this._input.length - 1].remove();
+				let top:number ;
+				this.DropdownMenu(["Facile", "Moyen", "Hard"], 0, [150, 40, -70, (40 * this._button.length) , "Arrial", "Choose your LVL"], (val) => {
+					this.displayButtonCustom(0, 20, 20, 40, 0, "Courier", "OK");
+				});
+			});
+		}
+			this._outline.remove();
+			this._texte.remove();
+			
+			this.Tournoi();
+		});
+	}
+
+
+	// public Tournoi() //trouver comment enlever le padding et recalculer pour tout bien psotionner
 	// {
 	// 	let std: string[] = ["3", "4", "5", "6", "7", "8"];
 	
-	// 	console.log("Menu = ", 50 + (50 * this.n));
+	// 	//console.log("Menu = ", 50 + (50 * this.n));
 	// 	this.displayMenuCustom(400, 50 + (50 * this.n));
 
 	// 	this.TextDisplayCustom(80, 25, -150, (-25 * this.n), "Arial", "Choose your number of players: ");
-	// 	console.log("(-25 * this.n) = ",(-25 * this.n));
+	// 	//console.log("(-25 * this.n) = ",(-25 * this.n));
 
-	// 	console.log("");
-	// 	this.DropdownMenu(std, 0, [80, 30, 115, ((-25 * this.n)), "Arial", `${this.n}`], (val) => {
-	// 		if (this.n != 0)
+	// 	//console.log("");
+	// 	this.DropdownMenu(["3", "4", "5", "6", "7", "8"], 0, [80, 30, 115, ((-25 * this.n)), "Arial", `${this.n + 1}`], (val) => {
+	// 		//if (this.n != 0)
 	// 			for (let x = 0; x < this._container.length; x++) {
 	// 				this._container[x].remove(); }
 
@@ -409,48 +488,28 @@ export class menu {
 	// 			let sup: number = 0
 	// 			if (this.n != 2)
 	// 				sup = 5 * (Number(val) - 2);
-	// 			// console.log("sup = ", sup);
-	// 			// console.log("(-20 * (Number(val))) = ", (-20 * (Number(val))), " + (50 * i) = ", 50 * i, "ALL = ", (-20 * (Number(val))) + (50 * i));
-	// 			this.DropdownMenu(std, i, [220, 40, -80, (-20 * (Number(val)) - sup) + (50 * i)  , "Arial", `${i} | Choose your opponent`], (val) => {}); }
+	// 			//console.log("sup = ", sup);
+	// 			//console.log("(-20 * (Number(val))) = ", (-20 * (Number(val))), " + (50 * i) = ", 50 * i, "ALL = ", (-20 * (Number(val))) + (50 * i));
+	// 			this.DropdownMenu(["Player", "IA"], i, [220, 40, -80, (-20 * (Number(val)) - sup) + (50 * i)  , "Arial", `${i} | Choose your opponent`], (val) => {
+	// 				//this._buttonMenu.textContent = String(val);
+	// 				console.log("Number(val) = ", String(val));
+	// 				this._button.style.padding = "0px";
+
+	// 				let top: number = window.innerHeight /  2 - this._container[i].offsetTop;
+	// 				console.log("Top = ", top);
+	// 				console.log("I = ", i);
+	// 				//this._container[i].offsetTop;
+
+	// 				this.createTextInputCustom(140, 20, -90, top, "Courier", "Pseudo de l'invite");
+	// 				this._container[i].remove();
+					
+	// 			}); }
 
 	// 		this._outline.remove();
 	// 		this._texte.remove();
-	// 		this._buttonMenu.remove();
 			
 	// 		this.Tournoi();
 	// 	});
 	// }
-	public Tournoi() //trouver comment enlever le padding et recalculer pour tout bien psotionner
-	{
-		let std: string[] = ["3", "4", "5", "6", "7", "8"];
-	
-		console.log("Menu = ", 50 + (50 * this.n));
-		this.displayMenuCustom(400, 50 + (50 * this.n));
-
-		this.TextDisplayCustom(80, 25, -150, (-25 * this.n), "Arial", "Choose your number of players: ");
-		console.log("(-25 * this.n) = ",(-25 * this.n));
-
-		console.log("");
-		this.DropdownMenu(std, 0, [80, 30, 115, ((-25 * this.n)), "Arial", `${this.n + 1}`], (val) => {
-			//if (this.n != 0)
-				for (let x = 0; x < this._container.length; x++) {
-					this._container[x].remove(); }
-
-		this.n = Number(val) - 1;
-			
-			for (let i = 1; i < Number(val); i++) {
-				let sup: number = 0
-				if (this.n != 2)
-					sup = 5 * (Number(val) - 2);
-				console.log("sup = ", sup);
-				console.log("(-20 * (Number(val))) = ", (-20 * (Number(val))), " + (50 * i) = ", 50 * i, "ALL = ", (-20 * (Number(val))) + (50 * i));
-				this.DropdownMenu(std, i, [220, 40, -80, (-20 * (Number(val)) - sup) + (50 * i)  , "Arial", `${i} | Choose your opponent`], (val) => {}); }
-
-			this._outline.remove();
-			this._texte.remove();
-			
-			this.Tournoi();
-		});
-	}
 
 }
